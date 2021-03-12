de1c3e26f113489459d3f26033306ae899147ad4
Lorenzo Bernini <templarec@gmail.com>
2021-03-12 16:49:37 +0100

    Esercizio completato

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
+
+
+
+// carica script quando ha finito di caricare la pagina
 $(document).ready(function () {
+	// variabili dei vari selettori
 	var menu = $(".nav-right ul li span");
-	menu.hover(function () {
-		$(this).siblings().toggleClass("active");
+	var drop = $(".nav-drop");
+	var body = $("body");
+	//quando il mouse va sullo span
+	menu.mouseenter(function () {
+		$(this).siblings().addClass("active");
+	});
+	//quando mouse esce dallo span
+	menu.mouseleave(function () {
+		$(this).siblings().removeClass("active");
+	});
+	//quando mouse esce dal menu dropdown
+	drop.mouseleave(function () {
+		$(this).removeClass("active");
+	});
+	//quando mouse entra nel menu dropdown per farlo rimanere visibile quando esce
+	//dallo span se l'utente vuole cliccare il link
+	drop.mouseenter(function () {
+		$(this).addClass("active");
+	});
+	//quando click sul body chiude dropdown
+	body.click(function () {
+		drop.removeClass("active");
 	})
+
+
 });
